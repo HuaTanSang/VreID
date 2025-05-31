a84b1af0a81378dc7b92e7489a29df0e96e009d6
HuaTanSang <huatansang2004@gmail.com>
2025-06-01 00:07:50 +0700

    modify streaming pipeline

--- streaming.py
+++ streaming.py
@@ -16,20 +16,16 @@ def load_kafka_config(config_path):
     with open(config_path, 'r') as file:
         return yaml.safe_load(file)
 
-# Khởi tạo YOLO model
 model = YOLO('./yolov8n.pt')
 
-# Khởi tạo Spark
 spark = SparkSession.builder \
     .appName("Use Yolo") \
     .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.1") \
     .getOrCreate()
 
-# Annotators
 box_annotator = sv.BoxAnnotator()
 label_annotator = sv.LabelAnnotator()
 
-# Hàm xử lý frame
 def process_frame(frame_bytes):
     arr = np.frombuffer(frame_bytes, dtype=np.uint8)
     frame = cv2.imdecode(arr, cv2.IMREAD_COLOR)
@@ -45,25 +41,22 @@ def process_frame(frame_bytes):
     _, buf = cv2.imencode('.jpg', out)
     return buf.tobytes()
 
-# Đăng ký UDF
 process_udf = udf(process_frame, BinaryType())
 
-# Đọc stream từ Kafka topic 'read'
 spark_df = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
     .option("subscribe", "read") \
-    .option("startingOffsets", "earliest") \
+    .option("startingOffsets", "latest") \
     .load()
 
-# Áp dụng UDF lên cột 'value'
 spark_df = spark_df.withColumn("value", process_udf("value"))
 
-# Ghi xuống Kafka topic 'write'
 query = spark_df.writeStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
     .option("topic", "write") \
     .option("checkpointLocation", "./checkpoint/write") \
+    .trigger(processingTime='1 seconds')\
     .start()
 
 query.awaitTermination()
